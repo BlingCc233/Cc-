0272c194eb9d733cd032e61b48138a065a59c52d
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-02-27 19:46:04 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -291,7 +291,7 @@ $('#switchmore').on('click', function () {
     } else {
         $('#container').attr('class', 'container');
         $("#change").html("Hello&nbsp;World&nbsp;!");
-        $("#change1").html("18岁，是大学生");
+        $("#change1").html("21岁，是大学生");
     }
 });
 
@@ -340,7 +340,7 @@ window.addEventListener('load', function () {
             //移动端隐藏更多页面
             $('#container').attr('class', 'container');
             $("#change").html("Hello&nbsp;World&nbsp;!");
-            $("#change1").html("18岁，是大学生");
+            $("#change1").html("21岁，是大学生");
 
             //移动端隐藏弹窗页面
             $('#box').css("display", "none");
